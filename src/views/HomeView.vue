b1268188e47fb5e1d1550672b1dae909dfba3ad8
Acipu <akun.asifurrohman@gmail.com>
2026-03-16 14:40:12 +0700

    initial commit maybe

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 class="text-4xl font-medium">
+        Event Booking App
+    </h1>
+    <h2 class="text-2xl font-medium">
+        All Events
+    </h2>
+    <h2 class="text-2xl font-medium">
+        Your Bookings
+    </h2>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
